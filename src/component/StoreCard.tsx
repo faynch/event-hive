0e7f56f1c4436b6b21a6e2a949b32723ca885a52
faynch <83182929+faynch@users.noreply.github.com>
2023-03-26 17:52:27 +0700

    top stores section

--- src/component/StoreCard.tsx
+++ src/component/StoreCard.tsx
@@ -1,14 +1,20 @@
 import Image from 'next/image'
+import GroupButton from './GroupButton'
 import Shop from '../pages/assets/shop.svg'
 
 const StoreCard = () => {
     return (
-        <div className="flex flex-col items-center gap-3 text-center rounded-lg bg-white mx-6 my-3 p-9">
-            <Image className='w-32' src={Shop} alt={''} />
-            <h5 className='font-extrabold text-lg'>STORE NAME</h5>
-            <button className='rounded-md bg-icon text-icon'>contact</button>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, voluptat</p>
-            <button className='rounded-xl bg-[#F5EAEA] text-[#F5EAEA]'>contacnnnt</button>
+        <div className="my-3 flex flex-col items-center gap-3 rounded-lg bg-white p-9 text-center">
+            <Image className="w-32" src={Shop} alt={''} />
+            <h5 className="text-lg font-extrabold">STORE NAME</h5>
+            <GroupButton />
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dolorem, voluptat
+            </p>
+            <button className="rounded-xl bg-[#F5EAEA] px-3 text-[#F16767]">
+                catagories
+            </button>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,14 +10,19 @@ export default function Home() {
             <Navbar />
             <Hero />
             {/* <Carousel /> */}
-            <section className="grid grid-cols-1 items-center justify-left bg-[#F3F4F4] md:grid-cols-2 lg:grid-cols-3">
-                {/* <h3 className="text-2xl font-extrabold text-primary">
+            <h3 className="text-2xl font-extrabold text-primary bg-[#F3F4F4] text-center py-3">
                     TOP STORES
-                </h3> */}
-                <StoreCard />
-                <StoreCard />
-                <StoreCard />
-                
+                </h3>
+            <section className="col-span-4 grid grid-cols-1 bg-[#F3F4F4] px-8 md:grid-cols-4 lg:px-16">
+                <div className="md:col-span-2 md:col-start-2">
+                    <StoreCard />
+                </div>
+                <div className="md:col-span-2 md:pr-3">
+                    <StoreCard />
+                </div>
+                <div className="md:col-span-2 md:pl-3">
+                    <StoreCard />
+                </div>
             </section>
         </>
     )
