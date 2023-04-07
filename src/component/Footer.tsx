94b0c81148a944e803e53be9e71dea5c7db0dee3
faynch <83182929+faynch@users.noreply.github.com>
2023-04-07 17:05:25 +0700

    fix footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -9,34 +9,26 @@ import Email from '../pages/assets/email.svg'
 
 function Footer() {
     return (
-        <section className="grid md:grid-cols-2 justify-center gap-6 gap-x-24 bg-[#A459D121] p-14">
-            <div className="flex flex-row items-center justify-end ">
-                <Image className="w-16" src={EventHive} alt={'Event Hive'} />
-                <h3 className="ml-3 bg-gradient-to-r from-[#EF9323] to-[#5D3891] bg-clip-text text-3xl font-extrabold text-transparent">
-                    EVENT HIVE
-                </h3>
-            </div>
-
+        <section className="grid justify-center gap-y-8 bg-[#A459D121] p-14 md:grid-cols-3">
             <div>
-                <h5 className="mb-3 text-xl font-extrabold text-primary">
+                <h5 className="mb-3 text-center text-xl font-extrabold text-primary">
                     CONTACT US
                 </h5>
-                <div className="flex flex-col lg:flex-row">
-                    <div className="mb-2 flex flex-row ">
+                <div className="flex flex-col">
+                    <div className="mb-2 flex flex-row">
                         <button>
-                            <Image
-                                className="mx-3 h-5"
-                                src={Location}
-                                alt={''}
-                            />
+                            <Image className="mx-3 h-5" src={Location} alt={''} />
                         </button>
                         Hive101
+                    </div>
+                    <div className="mb-2 flex flex-row">
                         <button>
                             <Image className="mx-3 h-5" src={Phone} alt={''} />
                         </button>
                         000-1111111
                     </div>
-                    <div className="mb-2 flex flex-row ">
+
+                    <div className="mb-2 flex flex-row">
                         <button>
                             <Image className="mx-3 h-5" src={Email} alt={''} />
                         </button>
@@ -44,8 +36,13 @@ function Footer() {
                     </div>
                 </div>
             </div>
-
-            <div className="flex flex-col md:col-span-2">
+            <div className="flex flex-row items-center justify-center ">
+                <Image className="w-12" src={EventHive} alt={'Event Hive'} />
+                <h3 className="ml-3 bg-gradient-to-r from-[#EF9323] to-[#5D3891] bg-clip-text text-3xl font-extrabold text-transparent md:text-2xl lg:text-3xl">
+                    EVENT HIVE
+                </h3>
+            </div>
+            <div className="flex flex-col justify-center">
                 <div className="mb-3 flex flex-row justify-center gap-3">
                     <button>
                         <Image className="h-6" src={Facebook} alt={''} />
