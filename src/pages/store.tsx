6540cec5f339395617ce35fccbdade0b9c8b74bc
faynch <83182929+faynch@users.noreply.github.com>
2023-04-08 20:01:57 +0700

    create store page

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -1,14 +1,26 @@
 import Navbar from '../component/Navbar'
-
+import StoreCard from '../component/StoreCard'
 import Footer from '../component/Footer'
 
 function store() {
-  return (
-    <>
-    <Navbar />
-    <Footer />
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <div className="mx-12 mt-10 rounded-lg bg-gradient-to-r from-[#EF9323] to-[#5D3891] p-[4px]">
+                <input
+                    type="text"
+                    placeholder="Type store name..."
+                    className="input input-bordered max-w w-full rounded-md px-4 py-2"
+                />
+            </div>
+            <div className="grid grid-cols-1 px-16 my-5 md:grid-cols-2 md:gap-x-6 lg:px-20 xl:grid-cols-3">
+                <StoreCard />
+                <StoreCard />
+                <StoreCard />
+            </div>
+            <Footer />
+        </>
+    )
 }
 
 export default store
