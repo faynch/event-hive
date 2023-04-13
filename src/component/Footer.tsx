86e298145e5e7a4d032a2c4d0246cf5e03a6dac2
faynch <83182929+faynch@users.noreply.github.com>
2023-04-13 19:20:31 +0700

    fix footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -9,40 +9,14 @@ import Email from '../pages/assets/email.svg'
 
 function Footer() {
     return (
-        <section className="grid justify-center gap-y-8 bg-[#A459D121] p-14 md:grid-cols-3">
-            <div className='lg:pl-8'>
-                <h5 className="mb-3 text-center text-xl font-extrabold text-primary">
-                    CONTACT US
-                </h5>
-                <div className="flex flex-col">
-                    <div className="mb-2 flex flex-row">
-                        <button>
-                            <Image className="mx-3 h-5" src={Location} alt={''} />
-                        </button>
-                        Hive101
-                    </div>
-                    <div className="mb-2 flex flex-row">
-                        <button>
-                            <Image className="mx-3 h-5" src={Phone} alt={''} />
-                        </button>
-                        000-1111111
-                    </div>
-
-                    <div className="mb-2 flex flex-row">
-                        <button>
-                            <Image className="mx-3 h-5" src={Email} alt={''} />
-                        </button>
-                        admin@eventhive.com
-                    </div>
-                </div>
-            </div>
-            <div className="flex flex-row items-center justify-center ">
+        <section className="grid gap-y-8 bg-[#A459D121] py-14 lg:flex">
+            <div className="lg:basis-2/3 flex flex-row items-center justify-center lg:justify-end">
                 <Image className="w-12" src={EventHive} alt={'Event Hive'} />
-                <h3 className="ml-3 bg-gradient-to-r from-[#EF9323] to-[#5D3891] bg-clip-text text-3xl font-extrabold text-transparent md:text-2xl lg:text-3xl">
+                <h3 className="ml-3 bg-gradient-to-r from-[#EF9323] to-[#5D3891] bg-clip-text text-3xl font-extrabold text-transparent">
                     EVENT HIVE
                 </h3>
             </div>
-            <div className="flex flex-col justify-center">
+            <div className="lg:basis-1/3 flex flex-col justify-center">
                 <div className="mb-3 flex flex-row justify-center gap-3">
                     <button>
                         <Image className="h-6" src={Facebook} alt={''} />
@@ -55,6 +29,31 @@ function Footer() {
                     </button>
                 </div>
                 <div className="text-center">© 2023 Event hive</div>
+            </div>
+            <div className="lg:basis-2/3">
+                <h5 className="mb-2 text-center text-xl font-extrabold text-primary lg:text-left">
+                    CONTACT US
+                </h5>
+                <div className="flex flex-row justify-center lg:justify-start">
+                    {/* <div className="mb-2 flex flex-row"> */}
+                    <button>
+                        <Image className="ml-3 mr-1 h-4" src={Location} alt={''} />
+                    </button>
+                    Hive101
+                    {/* </div> */}
+                    {/* <div className="mb-2 flex flex-row"> */}
+                    <button>
+                        <Image className="ml-2 mr-1 h-4" src={Phone} alt={''} />
+                    </button>
+                    000-111111
+                    {/* </div> */}
+                    {/* <div className="mb-2 flex flex-row"> */}
+                    <button>
+                        <Image className="ml-2 mr-1 h-4" src={Email} alt={''} />
+                    </button>
+                    admin@eventhive
+                    {/* </div> */}
+                </div>
             </div>
         </section>
     )
