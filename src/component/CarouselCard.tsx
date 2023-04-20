884febe3bde76cae868c02546fa9fedba96b314a
faynch <83182929+faynch@users.noreply.github.com>
2023-04-20 19:36:37 +0700

    Update CarouselCard.tsx

--- src/component/CarouselCard.tsx
+++ src/component/CarouselCard.tsx
@@ -1,49 +1,107 @@
 import Image from 'next/image'
 import Event from '../pages/assets/event.svg'
+import GroupButton from './GroupButton'
+
 const CarouselCard = () => {
-  return (
-    
-        <div id="default-carousel" className="relative" data-carousel="static">
-            {/* <!-- Carousel wrapper --> */}
-            <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+    return (
+        <>
+            <div className="bg-white py-8">
+                <h3 className="py-4 text-center text-2xl font-extrabold text-primary">
+                    UPCOMING EVENT
+                </h3>
+                <div className="carousel ">
+                    <div id="slide1" className="carousel-item relative w-full">
+                        <div className="grid grid-cols-1 gap-4 justify-items-center content-center">
+                            <Image
+                                src={Event}
+                                className="w-48 md:w-64"
+                                alt={''}
+                            />
 
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <span className="absolute text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
-                    <Image src={Event} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                            <div className="flex flex-col gap-4 pb-2">
+                                <h4 className="text-center text-xl font-extrabold">
+                                    EVENT NAME
+                                </h4>
+                                <p className="text-center text-[#989898] ">
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Consequatur, s
+                                </p>
+
+                                <GroupButton />
+                            </div>
+                        </div>
+                        <div className="absolute left-5 right-5 top-1/2 hidden -translate-y-1/2 transform justify-between md:flex">
+                            <a href="#slide4" className="btn-circle btn">
+                                ❮
+                            </a>
+                            <a href="#slide2" className="btn-circle btn">
+                                ❯
+                            </a>
+                        </div>
+                    </div>
+                    <div id="slide2" className="carousel-item relative w-full">
+                        <Image src={Event} className="w-full" alt={''} />
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                            <a href="#slide1" className="btn-circle btn">
+                                ❮
+                            </a>
+                            <a href="#slide3" className="btn-circle btn">
+                                ❯
+                            </a>
+                        </div>
+                    </div>
+                    <div id="slide3" className="carousel-item relative w-full">
+                        <Image src={Event} className="w-full" alt={''} />
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                            <a href="#slide2" className="btn-circle btn">
+                                ❮
+                            </a>
+                            <a href="#slide4" className="btn-circle btn">
+                                ❯
+                            </a>
+                        </div>
+                    </div>
+                    <div id="slide4" className="carousel-item relative w-full">
+                        <Image src={Event} className="w-full" alt={''} />
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                            <a href="#slide3" className="btn-circle btn">
+                                ❮
+                            </a>
+                            <a href="#slide1" className="btn-circle btn">
+                                ❯
+                            </a>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <Image src={Event} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-                </div>
-
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <Image src={Event} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                <div className="flex justify-center gap-2 py-2">
+                    <a href="#slide1">
+                        <button
+                            type="button"
+                            className="h-3 w-3 rounded-full bg-[#F3F4F4]"
+                        ></button>
+                    </a>
+                    <a href="#slide2">
+                        <button
+                            type="button"
+                            className="h-3 w-3 rounded-full bg-[#F3F4F4]"
+                        ></button>
+                    </a>
+                    <a href="#slide3">
+                        <button
+                            type="button"
+                            className="h-3 w-3 rounded-full bg-[#F3F4F4]"
+                        ></button>
+                    </a>
+                    <a href="#slide4">
+                        <button
+                            type="button"
+                            className="h-3 w-3 rounded-full bg-[#F3F4F4]"
+                        ></button>
+                    </a>
                 </div>
             </div>
-
-            {/* <!-- Slider indicators --> */}
-            <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-            </div>
-
-            {/* <!-- Slider controls --> */}
-            <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-                    <span className="sr-only">Previous</span>
-                </span>
-            </button>
-            <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-                    <span className="sr-only">Next</span>
-                </span>
-            </button>
-        </div>
-
-  )
+        </>
+    )
 }
 
 export default CarouselCard
