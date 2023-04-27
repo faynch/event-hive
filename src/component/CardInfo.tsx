633e052b5ce572b35460a38bd75ad13ba65faa84
Athicha Popa <athichaneo@gmail.com>
2023-04-28 00:07:35 +0700

    fix the edit shopinfo

--- src/component/CardInfo.tsx
+++ src/component/CardInfo.tsx
@@ -8,7 +8,9 @@ import { useState } from 'react'
 
 const LargeCard = () => {
     const [storeName, setStoreName] = useState('Example Shop')
-    const [description, setDescription] = useState('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis distinctio nostrum aliquid enim facere obcaecati in vero quia? Maxime nam dolore perspiciatis expedita quia tempora, consectetur deserunt. Mollitia, veritatis maiores?')
+    const [description, setDescription] = useState(
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis distinctio nostrum aliquid enim facere obcaecati in vero quia? Maxime nam dolore perspiciatis expedita quia tempora, consectetur deserunt. Mollitia, veritatis maiores?'
+    )
     const [editMode, setEditMode] = useState(false)
     const [like, setLike] = useState(false)
 
@@ -19,18 +21,31 @@ const LargeCard = () => {
 
     if (editMode) {
         return (
-            <div className="flex flex-row rounded-lg bg-white py-12 px-12 sm:px-20 xl:px-28 lg:max-w-7xl">
-                <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-16">
-                    <div>
-                        <Image className="basis-1/3 w-52 self-center sm:self-start" src={Shop} alt={''} />
-                    </div>
-                    <div className="basis-2/3 col-span-2 flex flex-col items-center sm:items-start gap-4">
-                        <input className="bg-white sm:text-4xl text-2xl font-extrabold" value={storeName} onChange={(e) => setStoreName(e.target.value)} />
+            <div className="relative flex flex-row rounded-lg bg-white py-12 px-12 sm:px-20 lg:max-w-7xl xl:px-28">
+                <div className="flex flex-col items-center gap-8 lg:flex-row lg:gap-16">
+                    <Image
+                        className="w-52 basis-1/3 self-center sm:self-start"
+                        src={Shop}
+                        alt={''}
+                    />
+
+                    <div className="col-span-2 flex basis-2/3 flex-col items-center gap-4 sm:items-start">
+                        <input
+                            className="border-2 border-black bg-white text-2xl font-extrabold sm:text-4xl"
+                            value={storeName}
+                            onChange={(e) => setStoreName(e.target.value)}
+                        />
                         <GroupButton />
-                        <button className="items-center sm:self-start rounded-xl bg-[#F5EAEA] px-3 text-[#F16767]">
+                        <button className="items-center rounded-xl bg-[#F5EAEA] px-3 text-[#F16767] sm:self-start">
                             catagories
                         </button>
-                        <textarea className='bg-white text-center sm:text-start w-full' value={description} onChange={(e) => setDescription(e.target.value)} />
+                        <textarea
+                            className="w-full bg-white text-center sm:text-start"
+                            rows={5}
+                            cols={100}
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
                         <button onClick={handleSave}>Save</button>
                     </div>
                 </div>
@@ -39,15 +54,20 @@ const LargeCard = () => {
     }
 
     return (
-        <div className="flex relative flex-row rounded-lg bg-white py-12 px-12 sm:px-20 xl:px-28 lg:max-w-7xl">
-            <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-16">
-                <Image className="basis-1/3 w-52 self-center sm:self-start" src={Shop} alt={''} />
+        <div className="relative flex flex-row rounded-lg bg-white py-12 px-12 sm:px-20 lg:max-w-7xl xl:px-28">
+            <div className="flex flex-col items-center gap-8 lg:flex-row lg:gap-16">
+                <Image
+                    className="w-52 basis-1/3 self-center sm:self-start"
+                    src={Shop}
+                    alt={''}
+                />
                 <div className="absolute top-8 right-8 flex items-center">
                     <button
                         onClick={() => setEditMode(!editMode)}
                         className="mx-3 w-7 md:w-9"
-                        
-                    ><Image src={Edit} alt={''} /></button>
+                    >
+                        <Image src={Edit} alt={''} />
+                    </button>
                     <button
                         onClick={() => setLike(!like)}
                         className="w-6 md:w-8"
@@ -60,16 +80,15 @@ const LargeCard = () => {
                     </button>
                 </div>
 
-                <div className="basis-2/3 col-span-2 flex flex-col items-center sm:items-start gap-4">
-                    <h2 className="sm:text-4xl text-2xl font-extrabold">{storeName}</h2>
+                <div className="col-span-2 flex basis-2/3 flex-col items-center gap-4 sm:items-start">
+                    <h2 className="text-2xl font-extrabold sm:text-4xl">
+                        {storeName}
+                    </h2>
                     <GroupButton />
                     <button className="items-center rounded-xl bg-[#F5EAEA] px-3 text-[#F16767] sm:self-start">
                         catagories
                     </button>
-                    <p className='text-center sm:text-start'>
-                        {description}
-                    </p>
-                    <button onClick={() => setEditMode(true)}>Edit</button>
+                    <p className="text-center sm:text-start">{description}</p>
                 </div>
             </div>
         </div>
