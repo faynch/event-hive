c0ace476aab3dec214e9a7e068a4ff55ba67383d
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2023-04-29 01:17:28 +0700

    create event default api

---
+++ src/pages/api/events/[id].ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    const { query } = req;
+    const { id } = query;
+    const prisma = new PrismaClient();
+    const event = await prisma.event.findMany({
+        where: {
+            OR: [
+                {
+                    id: {
+                        contains: Array.isArray(id)
+                        ? id[0]: id,
+                    }
+                }
+            ]
+        }
+    })
+}
