607b74036dd0f5f734d57a511207eca31319e6ff
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2023-04-30 22:32:31 +0700

    create eventOrganizer default api

---
+++ src/pages/api/eventorganizers/[id].ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    const { query } = req;
+    const { id } = query;
+    const prisma = new PrismaClient();
+    const eventOrganizer = await prisma.eventOrganizer.findMany({
+        where: {
+            OR: [
+                {
+                    id: {
+                        contains: Array.isArray(id)
+                        ? id[0]: id,
+                    }
+                }
+            ]
+        }
+    })
+
+    if(eventOrganizer === undefined || eventOrganizer.length == 0){
+        return res.status(404).json({message: 'The page does not exist'});
+    }
+    
+    return res.json(eventOrganizer);
+}
