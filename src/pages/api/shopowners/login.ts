145ba901943c4c57223c9325ae95ef4e74ed6c6b
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2023-05-03 00:26:24 +0700

    create shopOwner login api

---
+++ src/pages/api/shopowners/login.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    if(req.method != "GET"){
+        return res.status(405).json({message: 'Method is not allowed'});
+    }
+    try{
+        const { email, password } = req.body;
+        const prisma = new PrismaClient();
+
+        if(!email || !password){
+            return res.status(400).json({message: 'Please provide all required fields'});
+        }
+
+        const existingEmail = await prisma.shopOwner.findFirst({
+            where: {
+                email: email,
+            },
+        })
+
+        if(!existingEmail){
+            return res.status(400).json({message: 'The email does not exist'})
+        }
+        if(password !== existingEmail.password){
+            return res.status(200).json({message: 'The password is not correct'})
+        }
+        return res.status(200).json({message: 'The email and the password are matched'})
+    }catch(error){
+        return res.status(400).json({message: 'Something went wrong'})
+    }
+}
