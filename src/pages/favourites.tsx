dc35ca9d42f062c5379f3d2433921ccebb6d679f
faynch <83182929+faynch@users.noreply.github.com>
2023-05-24 22:42:36 +0700

    link favourite event

--- src/pages/favourites.tsx
+++ src/pages/favourites.tsx
@@ -4,7 +4,7 @@ import Footer from '../component/Footer'
 import Navigator from '../component/Navigator'
 import { useState } from 'react'
 
-function favourites() {
+function favourites({ eventdata }: any) {
     const [toggle, setToggle] = useState(false)
 
     return (
@@ -36,14 +36,31 @@ function favourites() {
                     </button>
                 </div>
                 <div className="mb-8 grid grid-cols-1 gap-8 place-self-center lg:max-w-7xl lg:grid-cols-2 xl:grid-cols-3">
-                    <Card type={toggle ? "Event" : "Shop"}/>
-                    <Card type={toggle ? "Event" : "Shop"}/>
-                    <Card type={toggle ? "Event" : "Shop"}/>
+                    {toggle ? (
+                        ''
+                    ) : (
+                        <>
+                            {eventdata.map((item: any) => (
+                                <Card type="Event" data={item} />
+                            ))}
+                        </>
+                    )}
                 </div>
             </div>
             <Footer />
         </>
     )
+}
+
+export async function getServerSideProps() {
+    const res = await fetch('http://localhost:3000/api/events/') // Replace with your API endpoint URL
+    const data = await res.json()
+
+    return {
+        props: {
+            eventdata: data,
+        },
+    }
 }
 
 export default favourites
