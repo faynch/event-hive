82b52e3d153468a7498babb9eaa0aec0b4eb7bd3
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2023-05-26 00:53:43 +0700

    update shops api: get all shops

--- src/pages/api/shops/index.ts
+++ src/pages/api/shops/index.ts
@@ -6,6 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const shops = await prisma.shop.findMany({
         include: {
             tags: true,
+            shopOwner: true,
             eventApplications: true,
             eventParticipations: true,
             favouriteByUsers: true,
