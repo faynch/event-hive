2a3d195018621e6e263eadce739a884276ca5729
Athicha Popa <athichaneo@gmail.com>
2023-05-28 18:04:43 +0700

    edit size

--- src/pages/register/organizer.tsx
+++ src/pages/register/organizer.tsx
@@ -82,100 +82,107 @@ export default function organizer() {
     }
     return (
         <>
-            <Navbar />
-            <div className="flex flex-col-reverse items-center justify-center px-3 py-12 lg:flex-row">
-                <div className="flex flex-col items-center gap-6 lg:pl-6">
-                    <h3 className="bg-clip-text text-2xl font-extrabold text-[#5D3891]">
-                        YOU ARE ...
-                    </h3>
-                    <div className="m-10 grid w-full max-w-[1080px] grid-cols-1 gap-5 lg:flex">
-                        <div>
-                            <RegisterCard
-                                image={Organzier}
-                                alt="Organzier"
-                                title="Organzier"
-                            />
-                        </div>
-                        <div className="w-full max-w-[500px] rounded-lg bg-[#F5EAEA] p-10 drop-shadow-lg">
-                            <div className="my-3">
-                                <h5 className="text-sm font-extrabold text-[#A459D1] lg:text-2xl">
-                                    COMPANY NAME
-                                </h5>
-                                <input
-                                    className="w-full rounded-md border border-slate-300 bg-white py-2 pl-2 pr-3 shadow-sm placeholder:text-slate-400"
-                                    placeholder="Company Name"
-                                    type="name"
-                                    name="companyname"
-                                    value={companyName}
-                                    onChange={handleCompanyNameChange}
-                                />
-                            </div>
-                            <div className="my-3">
-                                <h5 className="my-3 mr-10 text-sm font-extrabold text-[#A459D1] lg:text-2xl">
-                                    EMAIL
-                                </h5>
-                                <input
-                                    className="w-full rounded-md border border-slate-300 bg-white py-2 pl-2 pr-3 shadow-sm  placeholder:text-slate-400"
-                                    placeholder="Email"
-                                    type="email"
-                                    name="email"
-                                    value={email}
-                                    onChange={handleEmailChange}
-                                />
-                            </div>
-                            <div className="flex justify-end">
-                                {!isValidEmail && (
-                                    <p className="text-red-500">
-                                        Please enter a valid email address
-                                    </p>
-                                )}
-                            </div>
-                            <div className="my-3">
-                                <h5 className="my-3 mr-10 text-sm font-extrabold text-[#A459D1] lg:text-2xl">
-                                    PASSWORD
-                                </h5>
-                                <input
-                                    className="block w-full rounded-md border border-slate-300 bg-white py-2 pl-2 pr-3 shadow-sm  placeholder:text-slate-400"
-                                    placeholder="Password"
-                                    type="password"
-                                    name="password"
-                                    value={password}
-                                    onChange={handlePasswordChange}
-                                />
-                            </div>
-                            <div className="my-3">
-                                <h5 className="my-3 mr-10 text-sm font-extrabold text-[#A459D1] lg:text-2xl">
-                                    CONFIRM PASSWORD
-                                </h5>
-                                <input
-                                    className="block w-full rounded-md border border-slate-300 bg-white py-2 pl-2 pr-3 shadow-sm  placeholder:text-slate-400"
-                                    placeholder="Confirm Password"
-                                    type="password"
-                                    name="confirmPassword"
-                                    value={confirmPassword}
-                                    onChange={handleConfirmPasswordChange}
-                                />
-                            </div>
-                            <div className="flex justify-end">
-                                {!passwordMatch && (
-                                    <p className="text-red-500">
-                                        Passwords do not match.
-                                    </p>
-                                )}
-                            </div>
-                            <div className="flex justify-end">
-                                <button
-                                    className="justify-end rounded-lg bg-[#FFB84C] from-[#EF9323] to-[#5D3891] px-6 py-2 font-extrabold text-white hover:bg-gradient-to-r"
-                                    onClick={handleSubmit}
-                                >
-                                    Submit
-                                </button>
+            <div className="flex min-h-screen flex-col">
+                <Navbar />
+                <div className="flex-grow">
+                    <div className="flex flex-col-reverse items-center justify-center px-3 py-12 lg:flex-row">
+                        <div className="flex flex-col items-center gap-6 lg:pl-6">
+                            <h3 className="bg-clip-text text-2xl font-extrabold text-[#5D3891]">
+                                YOU ARE ...
+                            </h3>
+                            <div className="m-10 grid w-full max-w-[1080px] grid-cols-1 gap-5 lg:flex">
+                                <div>
+                                    <RegisterCard
+                                        image={Organzier}
+                                        alt="Organzier"
+                                        title="Organzier"
+                                    />
+                                </div>
+                                <div className="w-full max-w-[500px] rounded-lg bg-[#F5EAEA] p-10 drop-shadow-lg">
+                                    <div className="my-3">
+                                        <h5 className="text-sm font-extrabold text-[#A459D1] lg:text-2xl">
+                                            COMPANY NAME
+                                        </h5>
+                                        <input
+                                            className="w-full rounded-md border border-slate-300 bg-white py-2 pl-2 pr-3 shadow-sm placeholder:text-slate-400"
+                                            placeholder="Company Name"
+                                            type="name"
+                                            name="companyname"
+                                            value={companyName}
+                                            onChange={handleCompanyNameChange}
+                                        />
+                                    </div>
+                                    <div className="my-3">
+                                        <h5 className="my-3 mr-10 text-sm font-extrabold text-[#A459D1] lg:text-2xl">
+                                            EMAIL
+                                        </h5>
+                                        <input
+                                            className="w-full rounded-md border border-slate-300 bg-white py-2 pl-2 pr-3 shadow-sm  placeholder:text-slate-400"
+                                            placeholder="Email"
+                                            type="email"
+                                            name="email"
+                                            value={email}
+                                            onChange={handleEmailChange}
+                                        />
+                                    </div>
+                                    <div className="flex justify-end">
+                                        {!isValidEmail && (
+                                            <p className="text-red-500">
+                                                Please enter a valid email
+                                                address
+                                            </p>
+                                        )}
+                                    </div>
+                                    <div className="my-3">
+                                        <h5 className="my-3 mr-10 text-sm font-extrabold text-[#A459D1] lg:text-2xl">
+                                            PASSWORD
+                                        </h5>
+                                        <input
+                                            className="block w-full rounded-md border border-slate-300 bg-white py-2 pl-2 pr-3 shadow-sm  placeholder:text-slate-400"
+                                            placeholder="Password"
+                                            type="password"
+                                            name="password"
+                                            value={password}
+                                            onChange={handlePasswordChange}
+                                        />
+                                    </div>
+                                    <div className="my-3">
+                                        <h5 className="my-3 mr-10 text-sm font-extrabold text-[#A459D1] lg:text-2xl">
+                                            CONFIRM PASSWORD
+                                        </h5>
+                                        <input
+                                            className="block w-full rounded-md border border-slate-300 bg-white py-2 pl-2 pr-3 shadow-sm  placeholder:text-slate-400"
+                                            placeholder="Confirm Password"
+                                            type="password"
+                                            name="confirmPassword"
+                                            value={confirmPassword}
+                                            onChange={
+                                                handleConfirmPasswordChange
+                                            }
+                                        />
+                                    </div>
+                                    <div className="flex justify-end">
+                                        {!passwordMatch && (
+                                            <p className="text-red-500">
+                                                Passwords do not match.
+                                            </p>
+                                        )}
+                                    </div>
+                                    <div className="flex justify-end">
+                                        <button
+                                            className="justify-end rounded-lg bg-[#FFB84C] from-[#EF9323] to-[#5D3891] px-6 py-2 font-extrabold text-white hover:bg-gradient-to-r"
+                                            onClick={handleSubmit}
+                                        >
+                                            Submit
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <Footer />
             </div>
-            <Footer />
         </>
     )
 }
