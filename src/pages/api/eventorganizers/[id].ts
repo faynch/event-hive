915d1a92ac918edc46c3ca8d9838338eebb52256
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2023-05-28 15:11:20 +0700

    change from login Link to Div

--- src/pages/api/eventorganizers/[id].ts
+++ src/pages/api/eventorganizers/[id].ts
@@ -24,6 +24,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if(eventOrganizer === undefined || eventOrganizer.length == 0){
         return res.status(404).json({message: 'The page does not exist'});
     }
+
+    if(req.method == "GET"){
+        return res.json(eventOrganizer);
+    }
     
-    return res.json(eventOrganizer);
+    if(req.method == "POST"){
+        const { companyName, email, password, events } = req.body;
+    }
 }
