5d14b3c5fc6b60f9b7ea0e3ce430d9139890adf7
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2023-05-29 16:37:39 +0700

    change from some to every

--- src/pages/api/shopsbytags.ts
+++ src/pages/api/shopsbytags.ts
@@ -15,7 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const shops = await prisma.shop.findMany({
     where: {
         tags: {
-            some:{
+            every:{
                 id: {
                     in: tagIds,
                 },
