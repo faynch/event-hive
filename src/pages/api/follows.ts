0dc47ab26f1e4600c6e9acc086c8aded51aeadc8
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2023-06-05 20:51:15 +0700

    update follow/unfollow

--- src/pages/api/follows.ts
+++ src/pages/api/follows.ts
@@ -1,48 +1,56 @@
-import { PrismaClient } from '@prisma/client';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getSession } from 'next-auth/react';
+import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  }
-  console.log("is it here")
-  
-  const { type, targetId, userId } = req.body; 
-
-  if (!type || !targetId || !userId) {
-    return res.status(400).json({ message: 'Invalid Request' });
-  }
-
-  try {
-    let user;
-    if (type === 'shop') {
-        user = await prisma.visitor.update({
-        where: { id: userId },
-        data: {
-          favouriteShops: {
-            connect: { id: targetId },
-          },
-        },
-      });
-    } else if (type === 'event') {
-        user = await prisma.visitor.update({
-        where: { id: userId },
-        data: {
-          favouriteEvents: {
-            connect: { id: targetId },
-          },
-        },
-      });
-    } else {
-      return res.status(400).json({ message: 'Invalid Type' });
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method Not Allowed' })
     }
 
-    return res.status(200).json(user);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: 'Something went wrong' });
-  }
+    const { type, targetId, userId } = req.body
+
+    if (!type || !targetId || !userId) {
+        return res.status(400).json({ message: 'Invalid Request' })
+    }
+
+    try {
+        let user
+        if (type === 'shop') {
+            user = await prisma.visitor.update({
+                where: { id: userId },
+                data: {
+                    favouriteShops: {
+                        connect: { id: targetId },
+                    },
+                },
+                include: {
+                    favouriteShops: true,
+                },
+            })
+        } else if (type === 'event') {
+            user = await prisma.visitor.update({
+                where: { id: userId },
+                data: {
+                    favouriteEvents: {
+                        connect: { id: targetId },
+                    },
+                },
+                include: {
+                    favouriteEvents: true,
+                },
+            })
+        } else {
+            return res.status(400).json({ message: 'Invalid Type' })
+        }
+
+        return res.status(200).json(user)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: 'Something went wrong' })
+    }
 }
--- src/pages/api/unfollows.ts
+++ src/pages/api/unfollows.ts
@@ -1,63 +1,72 @@
-import { PrismaClient } from '@prisma/client';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getSession } from 'next-auth/react';
+import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  }
-
-  const { type, targetId, userId } = req.body; 
-
-  if (!type || !targetId || !userId) {
-    return res.status(400).json({ message: 'Invalid Request' });
-  }
-
-  try {
-    let user;
-    if (type === 'shop') {
-        const shop = await prisma.shop.update({
-        where: { id: targetId },
-        data: {
-          favouriteByVisitors: {
-            disconnect: { id: userId },
-          },
-        },
-    });
-    user = await prisma.visitor.update({
-      where: {id: userId},
-      data: {
-        favouriteShops: {
-          disconnect: {id:targetId},
-        },
-      },
-    });
-    } else if (type === 'event') {
-        const event = await prisma.event.update({
-        where: { id: targetId },
-        data: {
-          favouriteByVisitors: {
-            disconnect: { id: userId },
-          },
-        },
-      });
-      user = await prisma.visitor.update({
-        where: {id: userId},
-        data: {
-          favouriteEvents: {
-            disconnect: {id:targetId},
-          },
-        },
-      });
-    } else {
-      return res.status(400).json({ message: 'Invalid Type' });
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method Not Allowed' })
     }
-    
-    return res.status(200).json(user);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: 'Something went wrong' });
-  }
+
+    const { type, targetId, userId } = req.body
+
+    if (!type || !targetId || !userId) {
+        return res.status(400).json({ message: 'Invalid Request' })
+    }
+
+    try {
+        let user
+        if (type === 'shop') {
+            const shop = await prisma.shop.update({
+                where: { id: targetId },
+                data: {
+                    favouriteByVisitors: {
+                        disconnect: { id: userId },
+                    },
+                },
+            })
+            user = await prisma.visitor.update({
+                where: { id: userId },
+                data: {
+                    favouriteShops: {
+                        disconnect: { id: targetId },
+                    },
+                },
+                include: {
+                    favouriteShops: true,
+                },
+            })
+        } else if (type === 'event') {
+            const event = await prisma.event.update({
+                where: { id: targetId },
+                data: {
+                    favouriteByVisitors: {
+                        disconnect: { id: userId },
+                    },
+                },
+            })
+            user = await prisma.visitor.update({
+                where: { id: userId },
+                data: {
+                    favouriteEvents: {
+                        disconnect: { id: targetId },
+                    },
+                },
+                include: {
+                    favouriteEvents: true,
+                },
+            })
+        } else {
+            return res.status(400).json({ message: 'Invalid Type' })
+        }
+
+        return res.status(200).json(user)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: 'Something went wrong' })
+    }
 }
